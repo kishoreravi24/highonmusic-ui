fea742142c6e14aeb1cb6eeac7833a568c91a800
kishoreravi24 <kishoreravi24399@gmail.com>
2023-07-25 23:17:31 +0530

    done some grid works

---
+++ src/App/App.jsx
@@ -0,0 +1,21 @@
+import LeftPanel from '../LeftPanel/leftPanel';
+import MainPanel from '../MainPanel.jsx/mainPanel';
+import './App.css';
+import { Grid } from '@mui/material';
+
+function App() {
+  return (
+    <div className="App">
+      <Grid container spacing={2} style={{ margin: 0 }}>
+        <Grid item xs={6} and md={3}>
+          <LeftPanel/>
+        </Grid>
+        <Grid item xs={6} and md={9}>
+          <MainPanel/>
+        </Grid>
+      </Grid>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/MainPanel.jsx/mainPanel.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Box } from '@mui/material';
+
+
+function MainPanel() {
+    return (
+        <div>
+            <Box sx={{ bgcolor: '#cfe8fc', height: '100vh' }}>
+                <h1>Main Panel</h1>
+            </Box>
+        </div>
+    )
+}
+
+export default MainPanel;
