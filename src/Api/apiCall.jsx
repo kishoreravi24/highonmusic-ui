28276991675edd02ed287c9dca2ceb52f3b00494
kishoreravi24 <kishoreravi24399@gmail.com>
2023-07-28 13:20:52 +0530

    final fix

--- src/Api/apiCall.jsx
+++ src/Api/apiCall.jsx
@@ -1,7 +1,9 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, createContext } from 'react';
 
-const Apiprovider = () => {
-  const [audioURL, setAudioURL] = useState([]);
+const MyContext = createContext();
+
+const Apiprovider = ({children}) => {
+  const [data, setData] = useState([]);
   const songsApiUrl = "http://localhost:5000/songs/getSongs";
 
   useEffect(() => {
@@ -15,39 +17,19 @@ const Apiprovider = () => {
         console.log("Network failure");
         return;
       }
-
       const jsonData = await response.json();
-      const audioURLs = [];
-
-      for (const data of jsonData) {
-        const binaryData = atob(data.audio);
-        const arrayBuffer = new ArrayBuffer(binaryData.length);
-        const view = new Uint8Array(arrayBuffer);
-        for (let i = 0; i < binaryData.length; i++) {
-          view[i] = binaryData.charCodeAt(i);
-        }
-        const blob = new Blob([view], { type: 'audio/mpeg' });
-
-        // Create an Object URL for the Blob
-        const objectURL = URL.createObjectURL(blob);
-        audioURLs.push(objectURL);
-      }
-
-      // Set the audioURL state with all the URLs
-      setAudioURL(audioURLs);
+      console.log(jsonData);
+      setData(jsonData);
     } catch (error) {
       console.error('Error fetching data:', error);
     }
   };
 
-  return (
-    <div>
-      <h1>Song: {audioURL.length > 0 ? 'bae' : 'Loading...'}</h1>
-      {audioURL.map((url, index) => (
-        <audio key={index} controls src={url} />
-      ))}
-    </div>
-  );
+  return(
+    <MyContext.Provider value={{data,setData}}>
+        {children}
+    </MyContext.Provider>
+  )
 };
 
-export default Apiprovider;
+export {MyContext,Apiprovider};
--- src/App/App.jsx
+++ src/App/App.jsx
@@ -1,20 +1,60 @@
-import LeftPanel from '../LeftPanel/leftPanel';
-import MainPanel from '../MainPanel.jsx/mainPanel';
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import Tab from '@mui/material/Tab';
+import TabContext from '@mui/lab/TabContext';
+import TabList from '@mui/lab/TabList';
+import TabPanel from '@mui/lab/TabPanel';
+import { Container } from '@mui/material';
 import './App.css';
-import { Grid } from '@mui/material';
+import HighonSong from '../Songs/song';
+import HighonPodcast from '../Podcasts/podcasts';
+import HighonPlaylist from '../Playlists/playlists';
+import GraphicEqIcon from '@mui/icons-material/GraphicEq';
+import HeadsetIcon from '@mui/icons-material/Headset';
+import {Apiprovider} from '../Api/apiCall';
 
 function App() {
+  const [value, setValue] = React.useState(0);
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
   return (
-      <div className="App">
-        <Grid container spacing={2} style={{ margin: 0 }}>
-          <Grid item xs={6}  md={3}>
-            <LeftPanel />
-          </Grid>
-          <Grid item xs={6}  md={9}>
-            <MainPanel />
-          </Grid>
-        </Grid>
-      </div>
+    <div className="App">
+      <Apiprovider>
+      <Container maxWidth="xl">
+        <Box sx={{ bgcolor: 'whitesmoke', height: '100vh' }}>
+          <center>
+            <h1><HeadsetIcon/> HighonMusic <GraphicEqIcon/></h1>
+          </center>
+          <TabContext value={value}>
+            <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
+              <TabList onChange={handleChange} aria-label="lab API tabs example" centered>
+                <Tab label="Songs" value="1" />
+                <Tab label="Podcasts" value="2" />
+                <Tab label="Playlists" value="3" />
+              </TabList>
+            </Box>
+            <TabPanel value="1">
+              <center>
+                <HighonSong />
+              </center>
+            </TabPanel>
+            <TabPanel value="2">
+              <center>
+                <HighonPodcast />
+              </center>
+            </TabPanel>
+            <TabPanel value="3">
+              <center>
+                <HighonPlaylist />
+              </center>
+            </TabPanel>
+          </TabContext>
+        </Box>
+      </Container>
+      </Apiprovider>
+    </div>
   );
 }
 
