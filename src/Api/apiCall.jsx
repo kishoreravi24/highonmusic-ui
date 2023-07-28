46872332dba946ccac9c83a45c8bd1fd35c0abac
kishoreravi24 <kishoreravi24399@gmail.com>
2023-07-28 08:22:59 +0530

    token login and song pass

--- src/Api/apiCall.jsx
+++ src/Api/apiCall.jsx
@@ -1,48 +1,53 @@
-import {useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 
 const Apiprovider = () => {
-    const [data,SetData] = useState([]);
-    const [audioURL,setAudioURL] = useState([]);
-    const ApiUrl = "http://localhost:5000/songs/getSongs";
+  const [audioURL, setAudioURL] = useState([]);
+  const songsApiUrl = "http://localhost:5000/songs/getSongs";
 
-    useEffect(() => {
-        fetchData();
-    },[]);
+  useEffect(() => {
+    fetchData();
+  }, []);
 
-    const fetchData = async () => {
-        try {
-          const response = await fetch(ApiUrl);
-          if (!response.ok) {
-            console.log("Network failure");
-          } else {
-            const jsonData = await response.json();
-            console.log(jsonData);
-            console.log(jsonData[0].audio);
-      
-            // Access the audio data from jsonData instead of response.data
-            const binaryData = atob(jsonData[0].audio);
-            const arrayBuffer = new ArrayBuffer(binaryData.length);
-            const view = new Uint8Array(arrayBuffer);
-            for (let i = 0; i < binaryData.length; i++) {
-              view[i] = binaryData.charCodeAt(i);
-            }
-            const blob = new Blob([view], { type: 'audio/mpeg' });
-      
-            // Create an Object URL for the Blob
-            const objectURL = URL.createObjectURL(blob);
-            setAudioURL(objectURL);
-          }
-        } catch (error) {
-          console.error('Error fetching data:', error);
+  const fetchData = async () => {
+    try {
+      const response = await fetch(songsApiUrl);
+      if (!response.ok) {
+        console.log("Network failure");
+        return;
+      }
+
+      const jsonData = await response.json();
+      const audioURLs = [];
+
+      for (const data of jsonData) {
+        const binaryData = atob(data.audio);
+        const arrayBuffer = new ArrayBuffer(binaryData.length);
+        const view = new Uint8Array(arrayBuffer);
+        for (let i = 0; i < binaryData.length; i++) {
+          view[i] = binaryData.charCodeAt(i);
         }
-      };
+        const blob = new Blob([view], { type: 'audio/mpeg' });
 
-    return (
-        <div>
-          <h1>Song: {audioURL ? 'bae' : 'Loading...'}</h1>
-          {audioURL && <audio controls src={audioURL} />}
-        </div>
-      );
-}
+        // Create an Object URL for the Blob
+        const objectURL = URL.createObjectURL(blob);
+        audioURLs.push(objectURL);
+      }
+
+      // Set the audioURL state with all the URLs
+      setAudioURL(audioURLs);
+    } catch (error) {
+      console.error('Error fetching data:', error);
+    }
+  };
+
+  return (
+    <div>
+      <h1>Song: {audioURL.length > 0 ? 'bae' : 'Loading...'}</h1>
+      {audioURL.map((url, index) => (
+        <audio key={index} controls src={url} />
+      ))}
+    </div>
+  );
+};
 
 export default Apiprovider;
